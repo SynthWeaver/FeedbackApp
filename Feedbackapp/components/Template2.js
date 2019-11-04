89aa61d216be99918ef454623fb7836364e862f1
S.D. Joosten <sam.joosten@hva.nl>
2019-11-04 18:57:11 +0100

    Expanded template 2

--- Feedbackapp/components/Template2.js
+++ Feedbackapp/components/Template2.js
@@ -11,11 +11,34 @@ import {
     FlatList
 } from 'react-native';
 
+const features = [
+    {
+        type: 'option',
+        value: 'Template Feature 1'
+    },
+    {
+        type: 'option',
+        value: 'Template Feature 1'
+    },
+    {
+        type: 'option',
+        value: 'Template Feature 1'
+    },
+    {
+        type: 'option',
+        value: 'Template Feature 1'
+    },
+    {
+        type: 'input',
+        value: 'Other...'
+    }
+];
 class Template2 extends Component {
-    state = {};
+    state = {
+        loadTextInput: false
+    };
 
     componentDidMount() {
-        var data = [];
         var even = [];
         var uneven = [];
         for (var i = 0; i < 11; i++) {
@@ -25,7 +48,7 @@ class Template2 extends Component {
                 uneven.push({key: i, val: i});
             }
         }
-        data = even.concat(uneven);
+        var data = even.concat(uneven);
         this.setState({
             data: data
         })
@@ -33,10 +56,33 @@ class Template2 extends Component {
 
     renderItem = ({item}) => {
         return (
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.circleButton} onPress={() => {
+                if (item.val < 6) {
+                    this.setState({
+                        loadTextInput: true
+                    })
+                }
+            }}>
                 <Text style={{color: 'white', fontSize: 17, fontWeight: 'bold'}}>{item.val}</Text>
             </TouchableOpacity>
         )
+    }
+
+    renderButtonItem = ({item}) => {
+        if (item.type === 'option') {
+            return (
+                <TouchableOpacity style={[styles.button, {backgroundColor: 'white'}]}>
+                    <Text>{item.value}</Text>
+                </TouchableOpacity>
+            )
+        } else {
+            return (
+                <View style={styles.inputSection}>
+                    <TextInput placeholder={item.value}/>
+                </View>
+            )
+        }
+
     }
 
     render() {
@@ -56,10 +102,21 @@ class Template2 extends Component {
                     contentContainerStyle={styles.list}
                     data={this.state.data}
                     renderItem={this.renderItem}/>
-                <View style={{flex: 4, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-end'}}>
+                <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
                     <Text style={{margin: 10}}>Really Bad</Text>
                     <Text style={{margin: 10}}>Really Good</Text>
                 </View>
+                {this.state.loadTextInput ? <View style={styles.btnContainer}>
+                    <Text style={styles.featureText}>What feature did you not like?</Text>
+                    <FlatList numColumns={2}
+                              horizontal={false}
+                              contentContainerStyle={styles.btnList}
+                              data={features}
+                              renderItem={this.renderButtonItem}/>
+                </View> : <View style={styles.btnContainer}></View>}
+                <TouchableOpacity style={[styles.button, {backgroundColor: '#0984e3'}]}>
+                    <Text style={styles.btnText}>Submit!</Text>
+                </TouchableOpacity>
             </View>
         )
 
@@ -68,12 +125,22 @@ class Template2 extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flex: 6,
         backgroundColor: '#ecf0f1',
-        flexDirection: 'column-reverse',
+        flexDirection: 'column',
         justifyContent: 'center',
     },
-    button: {
+    btnContainer: {
+        flex: 3,
+        marginTop: 20,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    btnList: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    circleButton: {
         borderRadius: 100,
         margin: 3,
         backgroundColor: 'orange',
@@ -82,13 +149,39 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     },
+    button: {
+        borderRadius: 10,
+        borderWidth: 3,
+        padding: 20,
+        margin: 10,
+        borderColor: 'gray',
+        justifyContent: 'center'
+    },
+    featureText: {
+        fontSize: 30,
+        alignSelf: 'center'
+    },
+    inputSection: {
+        borderColor: 'gray',
+        borderWidth: 3,
+        borderRadius: 10,
+        backgroundColor: '#fff',
+        padding: 20,
+        margin: 10,
+        width: Dimensions.get('window').width - 55
+    },
     list: {
         flex: 1,
-        paddingBottom: 50,
+        // paddingBottom: 50,
         backgroundColor: '#ffffff',
         justifyContent: 'space-around',
         alignItems: 'center'
-    }
+    },
+    btnText: {
+        textAlign: 'center',
+        fontSize: 17,
+        color: 'white'
+    },
 })
 
 export default Template2
