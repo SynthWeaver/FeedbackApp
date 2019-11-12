d318978222167e2cf0c31992a16c9b6c35ea4b5b
S.D. Joosten <sam.joosten@hva.nl>
2019-11-12 14:18:41 +0100

    Added comments

--- Feedbackapp/components/Templates.js
+++ Feedbackapp/components/Templates.js
@@ -11,10 +11,10 @@ import Template3 from './Template3'
 import Constants from '../Constants'
 
 
+// Switcher to retrieve the template associated with the app and render the correct template
 class Templates extends Component {
     static navigationOptions = ({ navigation }) => {
         return {
-            // title: (navigation.getParam('appName') ? navigation.getParam('appName', 'Loading...') : navigation.getParam('name', 'Give Feedback'))
             headerTitle: () => <AppHeader appName={(navigation.getParam('appName') ? navigation.getParam('appName', 'Loading...') : navigation.getParam('name'))}
                                           appLogo={(navigation.getParam('appLogo') ? navigation.getParam('appLogo') : navigation.getParam('logo'))}/>
         };
@@ -31,6 +31,7 @@ class Templates extends Component {
 
 
     componentDidMount() {
+        // If the user opens an app through the deep link don't fetch from api
         if (!this.props.navigation.getParam('name')) {
             return fetch(this.state.url + 'get/apps/' + this.state.appId)
                 .then((response) => response.json())
@@ -59,6 +60,7 @@ class Templates extends Component {
         }
 
         var app = (this.state.data[0] ? this.state.data[0] : this.state.data);
+        // load the right template based on the template property from the app
         switch (app.template) {
             case "Template1" :
                 return <Template1 appName={app.appName} navigation={this.props.navigation}/>;
