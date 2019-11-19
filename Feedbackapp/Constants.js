3abc0bea008b5f2b37e2830baaf7251d93fdba11
Tim Terwijn <TimTerwijn@users.noreply.github.com>
2019-11-19 15:13:45 +0100

    Can create an account

--- Feedbackapp/Constants.js
+++ Feedbackapp/Constants.js
@@ -1,5 +1,9 @@
 export default class Constants {
-    static url = 'http://7cf324aa.ngrok.io/'
-    // local url for testing
-    //static url = 'http://localhost:8085/'
+    //don't forget slashes!!!!
+    
+    //heidi
+    //static url = 'http://7cf324aa.ngrok.io/'
+    
+    //tim
+    static url = 'http://67192b7e.ngrok.io/'
 }
