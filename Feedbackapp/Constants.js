1e3d86833c7805f41c34bba04722fae5d573eaec
CaptainLuca <likeabowzgameplays@gmail.com>
2019-11-19 10:39:53 +0100

    constants db ngrok

--- Feedbackapp/Constants.js
+++ Feedbackapp/Constants.js
@@ -1,5 +1,5 @@
 export default class Constants {
-    // static url = 'http://7a7333dc.ngrok.io/'
+     static url = 'http://de86a24f.ngrok.io/'
     // local url for testing
-    static url = 'http://localhost:8085/'
+    //static url = 'http://localhost:8085/'
 }
