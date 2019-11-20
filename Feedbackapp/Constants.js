bd271d596fca9ffc2d252d915bd431f3c61c30fd
samjoosten <31620260+samjoosten@users.noreply.github.com>
2019-11-20 13:48:43 +0100

    Finished initial version of choosing templates when registering

--- Feedbackapp/Constants.js
+++ Feedbackapp/Constants.js
@@ -1,9 +1,9 @@
 export default class Constants {
     //don't forget slashes!!!!
-    
+
     //heidi
     //static url = 'http://7cf324aa.ngrok.io/'
-    
+
     //tim
-    static url = 'http://67192b7e.ngrok.io/'
+    static url = 'http://localhost:8085/'
 }
